3452a68f605dff91e512d640af5e75d7656a7d61
NicoF46 <49656253+NicoF46@users.noreply.github.com>
2020-06-19 20:42:31 -0300

    Add endpoint for measure temperature (#3)

--- server/popos/connection.py
+++ server/popos/connection.py
@@ -2,17 +2,24 @@ import serial
 import serial.tools.list_ports as ports_list
 import struct
 
+SEPARATOR_SIZE = 1
+DATA_SIZE = 4
+
 class Connection(object):
     def __init__(self):
         self.connection_available = False
 
-    def create_connection(self, baud_rate, default_port = None):
+    def connect_device(self, baud_rate, device_name, default_port = None):
         ports = list(ports_list.comports())
         for p in ports:
-            if p.device == '/dev/ttyACM0' or p.device == '/dev/ttyUSB0':  # arduino found
-                print("Arduino found at " + p.device)
-                return serial.Serial(p.device, baud_rate)
-        raise IOError("Could not find an arduino - is it plugged in?")
+            if p.device == device_name:  # arduino found
+                print("Device found at " + p.device)
+                try:
+                    self.connection = serial.Serial(p.device, baud_rate)
+                    self.connection_available = True
+                    return 200
+                except:
+                    return 404
 
     def list_devices(self):
         devices = list(ports_list.comports())
@@ -22,10 +29,6 @@ class Connection(object):
             devices_info.append({"port": p.device, "description": p.description})
         return devices_info
 
-    def connect_device(self, baud_rate, device):
-        self.connection = serial.Serial(device, baud_rate)
-        self.connection_available = True
-
     def disconnect_device(self, device):
         if not self.connection_available:
             return { "error": "Can't close connection. None is open." }
@@ -34,6 +37,19 @@ class Connection(object):
             self.connection_available = False
             return { "status": "Closed Succesfully" }
         return { "error": "Can't close connection on port " + device }
+
+    def request_temperature_measure(self):
+        if not self.connection_available:
+            return { "error": "Device disconnected." }
+        self.connection.write(b'x')
+        while self.connection.in_waiting <(SEPARATOR_SIZE + DATA_SIZE):
+            pass
+        raw_separator = self.connection.read(SEPARATOR_SIZE)
+        if raw_separator == b'x':
+            serial_data = self.connection.read(DATA_SIZE)
+            temperature, = struct.unpack('<f', serial_data)
+            return {"temperature": str('%.1f'%(temperature))}
+        return { "error": "Invalid temperature measure." }        
 
     def ping(self, message = 'a' ):
         # raw_data = struct.pack('<cb', message)
