ada1280a1ed4c22ed8f272ba000a4104e6bf4801
Cecilia <cecilia.carbonaro@gmail.com>
2020-06-19 22:20:05 -0300

    Added graph. Added frequency measurement control. Changed temperature display widget. Added main window icon. Resized main window. *for resources to work one must modify mainwindow.py after using pyuic*

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from PyQt5 import QtGui, QtWidgets, QtCore, uic
 from server import flask_server
 import sys
 import requests
-from interface.design import Ui_ThermstUtil
+from interface.mainwindow import Ui_mainWindow
+import pyqtgraph as pg
 
 class Flask_server_thread(QtCore.QThread):
     port = int(flask_server.os.environ.get("PORT", 5000))
@@ -14,11 +15,18 @@ class Flask_server_thread(QtCore.QThread):
     def quit(self):
         requests.get("http://localhost:5000/shutdown")
 
-class MainWindow(QtWidgets.QMainWindow, Ui_ThermstUtil):
+class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.flask_thread = Flask_server_thread()
+        self.configurePlot()
+
+    def configurePlot(self):
+        color = self.palette().color(QtGui.QPalette.Base)
+        self.graphWidget.setBackground(color)
+        pen = pg.mkPen(color=(255, 0, 0), width=2)
+        self.graphWidget.plot([0,1,2,3,4,5,6,7,], [1,1,0,0,1,1,0,0], pen=pen)
 
 
 if __name__ == "__main__":
