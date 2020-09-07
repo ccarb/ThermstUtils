c4cd2184bd7d77157e679a132d97b2530f56ef5e
Franco <franco.accifonte@widergy.com>
2020-09-06 22:23:31 -0300

    finished commander recevie_data mehtods

--- server/popos/commander.py
+++ server/popos/commander.py
@@ -13,8 +13,7 @@ from pdb import set_trace as st
 class Commander():
     ser = None
 
-    def __init__(self, ser):
-        self.ser = ser
+    def __init__(self):
         self.commands = {}
         self.load_commands()
 
@@ -25,61 +24,54 @@ class Commander():
             self.commands[item["name"]] = Command(item, index)
     
     def hot(self, temp):
-        self.commands["hot"].perform([temp])
+        self.commands["hot"].perform(Commander.ser, [temp])
     
     def cold(self, temp):
-        self.commands["cold"].perform([temp])
+        self.commands["cold"].perform(Commander.ser, [temp])
 
     def status(self):
-        return self.commands["status"].perform()
+        return self.commands["status"].perform(Commander.ser, [])
     
     def read_temperature(self):
-        return self.commands["read_temperature"].perform()
+        return self.commands["read_temperature"].perform(Commander.ser, [])
 
 class Command():
     endianness = '<'
     parse_data_type = { "uint8_t": "b",
                         "float": "f" }
-    parse_unpac = { "uint8_t": "b",
-                    "float": "bbbb" }
+    data_size = { "uint8_t": 1,
+                  "float": 4 }
     data_formatter = { "uint8_t": int,
                        "float": float }
 
     def __init__(self, parameters, index):
         self.parameters = parameters
         self.name = parameters["name"]
-        self.sends_input = len(parameters["input"]) > 0
-        self.sends_output = len(parameters["output"]) > 0
         self.serial_identifier = bytes(str(index), encoding="ascii")
         self.send_data_format = self.endianness + 'c' # Char for the command identified by the array position.
         self.send_data_format += ''.join([ self.parse_data_type[a["type"]] for a in parameters["input"] ])
-        self.receive_data_format= ''.join([ self.parse_data_type[a["type"]] for a in parameters["output"] ])
+        self.receive_data_format = self.endianness + ''.join([ self.parse_data_type[a["type"]] for a in parameters["output"] ])
+        self.receive_data_buffer_size = sum([ self.data_size[a["type"]] for a in parameters["output"] ])
 
-
-    @classmethod
-    def send_chunk(cls, data_format, data):
-        st()
-        raw_data = struct.pack(data_format, *data)
-        Commander.ser.write(raw_data)
+    def send_chunk(self, serial_connection, data):
+        raw_data = struct.pack(self.send_data_format, self.serial_identifier, *data)
+        self.dato_enviado = raw_data
+        serial_connection.write(raw_data)
         return True
 
-    @classmethod
-    def read_chunk(cls, size, data_format):
-        raw_data = Commander.ser.read(size)
-        return struct.unpack(data_format, raw_data)
+    def read_chunk(self, serial_connection):
+        raw_data = serial_connection.read(self.receive_data_buffer_size)
+        return list(struct.unpack(self.receive_data_format, raw_data))
 
-    def perform(self, data): # No tengo idea porque, pero no me deja definirlo como classmethod
+    def perform(self, serial_connection, data):
         formated_data = self.format_input(data)
-        if self.sends_input: self.send_chunk(self.send_data_format, (self.serial_identifier, *formated_data))
-        if self.sends_output: return self.read_chunk(2, '<bb')
+        if len(self.parameters["input"]) > 0:
+            self.send_chunk(serial_connection, formated_data)
+        if len(self.parameters["output"]) > 0::
+            return self.read_chunk(serial_connection)
 
-    def format_input(self, data): # No tengo idea porque, pero no me deja definirlo como classmethod
+    def format_input(self, data):
         formated = []
         for index, variable in enumerate(self.parameters["input"]):
             formated.append(self.data_formatter[variable["type"]](data[index]))
         return tuple(formated)
-
-if __name__ == "__main__":
-    fa = Commander(1)
-    fa.cold(19)
-    st()
